7a4b3ab0896217a25497eb80d7d18efb18e1fc44
Bjorn Roche <bjorn@xowave.com>
2013-05-09 21:03:46 -0400

    added package doc files.

---
+++ src/com/xonami/javaBells/package-info.java
@@ -0,0 +1,7 @@
+/**
+This package is designed to make it easier to access Ice4J and libjitsi functionality from
+smack/xmpp code in java.
+
+@author Bjorn Roche
+*/
+package com.xonami.javaBells;
---
+++ src/com/xonami/javaBellsSample/package-info.java
@@ -0,0 +1,6 @@
+/**
+This package provides some simple sample code for the Java Bells project. It is not designed to be used on its own.
+
+@author Bjorn Roche
+*/
+package com.xonami.javaBellsSample;
